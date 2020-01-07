51d5a63ca3c83877c4ea2d9c66e86edd0a0dd36d
franshenrique <franshenrique@gmail.com>
2020-01-07 12:40:11 -0300

    Initial bottom bar

--- src/views/tutorials/details/Index.vue
+++ src/views/tutorials/details/Index.vue
@@ -1,13 +1,33 @@
 <template>
   <v-layout wrap justify-center align-center pa-5>
-    <v-card>
-        <v-card-title>Teste</v-card-title>
-    </v-card>
+      <v-bottom-navigation
+    v-model="bottomNav"
+    color="primary"
+    fixed
+  >
+    <v-btn>
+      <span>Informações</span>
+      <v-icon>info</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Casos</span>
+      <v-icon>list</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Notas</span>
+      <v-icon>assignment_turned_in</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
   </v-layout>
 </template>
 
 <script>
 export default {
   name: 'Tutorial',
+  data: () => ({
+    bottomNav: 0,
+  }),
 };
 </script>
