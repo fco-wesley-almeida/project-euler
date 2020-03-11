0589aa81d179a7ec4aef84b9660be092f0abf99a
Jagni <jagnidasa@gmail.com>
2020-03-11 20:18:06 -0300

    Adding image button

--- src/components/taiper_editor/clayblocks/Image.vue
+++ src/components/taiper_editor/clayblocks/Image.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout class="my-2">
+  <v-layout class="my-2" align-center justify-center wrap>
     <input
       ref="input"
       id="fileInput"
@@ -7,21 +7,21 @@
       accept="image/png, image/jpeg"
       @change="didChangeFile"
     />
-    <v-img style="cursor: pointer" contain ref="previewImage" @click="didTapFile" aspect-ratio="1" height="200" :src="image">
-      <template slot="placeholder">
-        <v-layout align-center fill-height justify-center>
-          <v-btn x-large color="primary">
-            Escolher imagem
-          <v-icon>image</v-icon>
-          </v-btn>
-        </v-layout>
-      </template>
-    </v-img>
+    <v-flex xs12 class="px-10">
+      <v-img v-if="image" style="cursor: pointer" contain ref="previewImage" @click="didTapFile"
+             aspect-ratio="1" :height="200" :src="image">
+      </v-img>
+    </v-flex>
+    <v-btn class="mt-2" @click="didTapFile" color="primary">
+      {{image ? 'Mudar imagem' : 'Escolher imagem'}}
+      <v-icon>image</v-icon>
+    </v-btn>
   </v-layout>
 </template>
 
 <script>
   import formRules from "@/utils/formRules";
+
   export default {
     mixins: [formRules],
     data: () => ({image: ''}),
@@ -30,22 +30,22 @@
         type: Object
       }
     },
-    mounted(){
-      if (typeof this.value.value == "string"){
+    mounted() {
+      if (typeof this.value.value == "string") {
         this.image = this.value.value;
       } else {
         if (this.value.value)
-        this.setPreviewImage(this.value.value);
+          this.setPreviewImage(this.value.value);
       }
     },
     methods: {
-      updateValue: function(value) {
+      updateValue: function (value) {
         this.$emit("input", value);
       },
-      didTapFile(){
+      didTapFile() {
         this.$refs.input.click();
       },
-      didChangeFile (e) {
+      didChangeFile(e) {
         let files = e.target.files || e.dataTransfer.files
         if (!files.length) return
         let file = files[0];
@@ -53,9 +53,9 @@
         this.setPreviewImage(file);
       },
       setPreviewImage(file) {
-        var _this = this
+        let _this = this;
 
-        var fr = new FileReader()
+        let fr = new FileReader()
 
         fr.onload = function () {
           _this.image = fr.result // is the data URL because called with readAsDataURL
