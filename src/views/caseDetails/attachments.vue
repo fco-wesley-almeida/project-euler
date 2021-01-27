ed8c6f27891103b0876fcb8c71eb1eeb0141fc8a
Wesley Almeida <fco.wesley.almeida@gmail.com>
2021-01-26 22:04:25 -0300

    feat: Capturar dados de anexos e mostrar dado bruto na tela de anexos

--- src/views/caseDetails/attachments.vue
+++ src/views/caseDetails/attachments.vue
@@ -1,3 +1,29 @@
 <template>
-    <p>TELA DE ANEXOS</p>
+  <v-layout fill-height align-center justify-center px-2>
+    <v-flex xs12 md8 lg6>
+      <v-card color="card">
+        <v-card-text class="pt-2 pb-10">
+          <p>{{attachments}}</p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+import TaiperEditor from "@/components/taiper_editor/index";
+export default {
+  components: { TaiperEditor },
+  props: {
+    tutorialCase: Object
+  },
+  data: () => ({
+
+  }),
+  computed: {
+      attachments () {
+        return this.tutorialCase.attachments    
+      }
+  }
+};
+</script>
