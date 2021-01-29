72b0af5bba7d4253c6c3f549a39ec30e96da663e
Wesley Almeida <fco.wesley.almeida@gmail.com>
2021-01-28 22:48:13 -0300

    feat: Mostrar dialog com formulário de inclusão de anexos

--- src/views/caseDetails/attachments.vue
+++ src/views/caseDetails/attachments.vue
@@ -1,6 +1,7 @@
 <template>
   <v-layout wrap justify-center align-center px-0>
     {{attachments}}
+    {{files}}
     <v-flex xs12 md10 lg8>
 
       <lister v-if="attachments"
@@ -16,21 +17,42 @@
           </div>
         </template>
       </lister>
+
+      <v-btn
+        class="mb-12"
+        color="primary"
+        fixed
+        bottom
+        right
+        fab
+        @click.native.stop="showingCreationDialog = true"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-dialog v-model="showingCreationDialog" transition="dialog-bottom-transition" scrollable>
+          <v-stepper class="card" non-linear v-model="stepper" vertical>
+            <v-stepper-content step="1">
+              <taiper-editor v-model="files"/>
+            </v-stepper-content>
+          </v-stepper>
+      </v-dialog>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import TaiperEditor from "@/components/taiper_editor/index.vue";
 import Lister from "@/components/Lister";
 import attachmentCard from "../../components/case/card/attachment";
 
 export default {
-  components: { Lister, attachmentCard },
+  components: { Lister, attachmentCard, TaiperEditor },
   props: {
     tutorialCase: Object
   },
   data: () => ({
-
+    files: [],
+    showingCreationDialog: false
   }),
   computed: {
       attachments () {
