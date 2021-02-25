da267103667ed38f9cd3560226874e47931eb26b
wesley <fco.wesley.almeida@gmail.com>
2021-02-24 21:52:23 -0300

    feat: Corrigir erros

--- src/views/caseDetails/open_steps.vue
+++ src/views/caseDetails/open_steps.vue
@@ -1,5 +1,12 @@
 <template>
-    <steps :stepsToRender="[1,2,3,4,5]" :tutorialCase="tutorialCase"/>
+    <steps 
+        :stepsToRender="[1,2,3,4,5]"
+        :tutorialCase="tutorialCase"
+        :messageAdvance="{
+            step: 5,
+            message: 'Dirija-se ao passo de fechamento.'
+        }"
+    />
 </template>
 
 <script>
--- src/views/caseDetails/steps.vue
+++ src/views/caseDetails/steps.vue
@@ -3,27 +3,41 @@
     <v-flex xs12 md8 lg6>
       <v-timeline dense style="margin-left: -25px">
         <v-timeline-item
-          :color="shouldHighlightStep(step.id - tutorialCase.currentStep + 1) || tutorialCase.status == 'finished' ? 'primary' : 'grey'"
+          :color="
+            shouldHighlightStep(step.id - tutorialCase.currentStep + 1) ||
+            tutorialCase.status == 'finished'
+              ? 'primary'
+              : 'grey'
+          "
           small
-          v-for="(step,i) in steps"
+          v-for="(step, i) in steps"
           v-bind:key="i"
         >
           <v-layout style="margin-left: -20px">
             <v-flex>
               <v-card color="card" class="mr-2">
-                <v-card-title :class="`headline ${shouldHighlightStep(step.id - tutorialCase.currentStep + 1) ? 'primary white--text' : ''} pt-2`">
+                <v-card-title
+                  :class="`headline ${
+                    shouldHighlightStep(step.id - tutorialCase.currentStep + 1)
+                      ? 'primary white--text'
+                      : ''
+                  } pt-2`"
+                >
                   Passo
-                  {{step.id}}
+                  {{ step.id }}
                   <v-spacer />
                 </v-card-title>
-                <v-card-text class="body-1 mt-3">{{stepDescription(step.id - 1)}}</v-card-text>
+                <v-card-text class="body-1 mt-3">{{
+                  stepDescription(step.id - 1)
+                }}</v-card-text>
                 <v-card-actions>
                   <v-spacer />
                   <v-btn
                     @click="stepTapped(step.id)"
                     :text="!$vuetify.theme.dark"
                     color="primary"
-                  >Ver respostas</v-btn>
+                    >Ver respostas</v-btn
+                  >
                 </v-card-actions>
               </v-card>
             </v-flex>
@@ -33,31 +47,58 @@
     </v-flex>
     <v-flex xs12>
       <v-layout justify-center align-center>
-        <v-btn v-if="allowAdvanceButton"
+        <v-btn
+          v-if="allowAdvanceButton"
           fixed
-          style="position: fixed; bottom: 0; right: 0; margin-right: 16px; margin-bottom: 72px"
+          style="
+            position: fixed;
+            bottom: 0;
+            right: 0;
+            margin-right: 16px;
+            margin-bottom: 72px;
+          "
           v-show="tutorialCase.status != 'finished'"
           @click="activationDialog = true"
           color="primary"
         >
-          {{advanceLabel}}
-          <v-icon class="ml-2">{{advanceIcon}}</v-icon>
+          {{ advanceLabel }}
+          <v-icon class="ml-2">{{ advanceIcon }}</v-icon>
         </v-btn>
       </v-layout>
     </v-flex>
-    <v-dialog v-model="activationDialog" transition="fade-transition" max-width="290">
+    <v-dialog
+      v-model="activationDialog"
+      transition="fade-transition"
+      max-width="290"
+    >
       <v-card color="card">
-        <v-card-title class="headline">{{advanceDialogTitle}}</v-card-title>
+        <v-card-title class="headline">{{ advanceDialogTitle }}</v-card-title>
 
-        <v-card-text>{{advanceDialogMessage}}</v-card-text>
+        <v-card-text>{{ advanceDialogMessage }}</v-card-text>
 
         <v-card-actions>
-          <v-btn color="grey darken-1" class="ml-2" text @click="activationDialog = false">Cancelar</v-btn>
+          <v-btn
+            color="grey darken-1"
+            class="ml-2"
+            text
+            @click="activationDialog = false"
+            >Cancelar</v-btn
+          >
           <v-spacer />
-          <v-btn color="primary" class="mr-2" text @click="didTapAdvance">Avançar</v-btn>
+          <v-btn color="primary" class="mr-2" text @click="didTapAdvance"
+            >Avançar</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-snackbar v-model="snackbar" color="primary" :timeout="-1">
+      {{ messageAdvance ? messageAdvance.message : '' }}
+      <template v-slot:action="{ attrs }">
+        <v-btn text :color="'white'" v-bind="attrs" @click="snackbar = false">
+          Ok
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-layout>
 </template>
 
@@ -73,42 +114,40 @@ export default {
     tutorialCase: Object,
     stepsToRender: {
       required: true,
-      type: Array
-    }
+      type: Array,
+    },
+    messageAdvance: {
+      required: false,
+      type: Object,
+    },
   },
   data: () => ({
     activationDialog: false,
-    finalStep: 7
-    // steps: [
-    //   {id: 7},
-    //   {id: 6},
-    //   {id: 5},
-    //   {id: 4},
-    //   {id: 3},
-    //   {id: 2},
-    //   {id: 1},
-    // ]
+    finalStep: 7,
+    snackbar: false,
   }),
   computed: {
-    steps () {
-      const filteredSteps = this.stepsToRender.reverse().filter(number => {
-        return number <= this.tutorialCase.currentStep
-      }) 
-      const steps = filteredSteps.map(number => {
+    steps() {
+      const filteredSteps = this.stepsToRender.reverse().filter((number) => {
+        return number <= this.tutorialCase.currentStep;
+      });
+      const steps = filteredSteps.map((number) => {
         return {
-          id: number
-        }
-      })
-      return steps
+          id: number,
+        };
+      });
+      return steps;
     },
-    allowAdvanceButton () {
-      const lastStep = this.stepsToRender[0]
-      const firstStep = this.stepsToRender.reverse()[0]
-      const { currentStep } = this.tutorialCase
-      return currentStep >= firstStep && currentStep <= lastStep
+    allowAdvanceButton() {
+      const lastStep = this.stepsToRender[0];
+      const firstStep = this.stepsToRender.reverse()[0];
+      const { currentStep } = this.tutorialCase;
+      return currentStep >= firstStep && currentStep <= lastStep;
     },
     advanceLabel() {
-      return this.tutorialCase.currentStep < this.finalStep ? "Avançar" : "Finalizar";
+      return this.tutorialCase.currentStep < this.finalStep
+        ? "Avançar"
+        : "Finalizar";
     },
     advanceIcon() {
       return this.tutorialCase.currentStep < this.finalStep ? "redo" : "check";
@@ -124,13 +163,16 @@ export default {
         return "Deseja realmente finalizar este passo? Os alunos não poderão mais enviar respostas.";
       }
       return "Deseja finalizar este caso? Os alunos não poderão mais enviar respostas ao passo 7 e você poderá tornar outro caso ativo.";
-    }
+    },
   },
   methods: {
     shouldHighlightStep(i) {
-      return i == 1 && this.tutorialCase.status != 'finished';
+      return i == 1 && this.tutorialCase.status != "finished";
     },
     didTapAdvance() {
+      if (this.messageAdvance && this.tutorialCase.currentStep === this.messageAdvance.step) {
+        this.snackbar = true;
+      }
       if (this.tutorialCase.currentStep == this.finalStep) {
         finishCase(this.$route.params.caseID);
       } else if (this.tutorialCase.currentStep > 1) {
@@ -144,7 +186,7 @@ export default {
       this.$store.state.app.currentStep = "Passo " + index;
       this.$router.push({
         name: "CaseStep",
-        params: { ...this.$route.params, step: index }
+        params: { ...this.$route.params, step: index },
       });
     },
     stepDescription(i) {
@@ -157,10 +199,10 @@ export default {
         "Estudo em casa e construção do mapa individual",
         "Construção e apresentação do mapa coletivo",
         "Artigo científico",
-        "Caso integrador"
+        "Caso integrador",
       ];
       return descriptions[i];
-    }
-  }
+    },
+  },
 };
 </script>
